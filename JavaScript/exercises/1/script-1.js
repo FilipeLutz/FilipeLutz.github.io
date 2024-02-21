29df9d6458d0d103c89dc4180370f9c27e9d84b0
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2024-02-21 14:30:01 +0000

    commit

--- JavaScript/exercises/1/script-1.js
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-// This is a single-line JavaScript comment
-
-=======
-// This is a single-line JavaScript comment
-
->>>>>>> 0db48f6a6470343552c492129b76244c22936917
-console.log("Hello from external JavaScript file.");
